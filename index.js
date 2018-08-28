3922cdbf38b2ce2e056436c12b6e86e0435b7497
Rendon Elsesser <rendonelsesser@gmail.com>
2018-08-28 14:36:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,35 @@ function theBeatlesPlay(musicians, instruments) {
   return emptyArray;
 }
 
+function johnLennonFacts()
+
+
+
+
+  describe('johnLennonFacts', function(){
+    it("returns an array of strings with exclamation points", function() {
+      expect(johnLennonFacts([
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
+
+      expect(johnLennonFacts([
+        "foo",
+        "bar",
+      ])).toEqual(["foo!!!", "bar!!!"])
+    });
+  });
+
+  describe('iLoveTheBeatles', function() {
+    it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
+      expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
+    });
+
+    it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
+      expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
+    });
+  });
+
+});
