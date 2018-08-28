783e39f063546dcf540ef8b7cf1d748855133338
Rendon Elsesser <rendonelsesser@gmail.com>
2018-08-28 15:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,17 @@ function johnLennonFacts(facts){
 }
 
 function iLoveTheBeatles()
+
+
+
+  describe('iLoveTheBeatles', function() {
+    it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
+      expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
+    });
+
+    it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
+      expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
+    });
+  });
+
+});
