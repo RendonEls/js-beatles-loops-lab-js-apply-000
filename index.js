b1bee5a21b81f9f7613356d9a1cc478417420822
Rendon Elsesser <rendonelsesser@gmail.com>
2018-08-28 14:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 
 function theBeatlesPlay(musicians, instruments) {
   var emptyArray = []
-  for (var i = 0; i < musicians.length; i++){
+  for (var i = 0; i < musicians.length; i++){   //length: to run the length of the database
     emptyArray.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return emptyArray;
 }
+
